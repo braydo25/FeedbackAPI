2213178a3c83b654bd94029fbd6c6c75ee83322a
Braydon Batungbacal <braydon@tryshared.com>
2020-10-21 02:46:20 -0700

    Updated suggestion algorithm for tracks.

--- routes/game.js
+++ routes/game.js
@@ -4,6 +4,8 @@
 
 const TrackModel = rootRequire('/models/TrackModel');
 const userAuthorize = rootRequire('/middlewares/users/authorize');
+const { shuffleArray } = rootRequire('/libs/utilityHelpers');
+
 const router = express.Router({
   mergeParams: true,
 });
@@ -18,15 +20,11 @@ router.get('/', asyncMiddleware(async (request, response) => {
   const scope = [
     'withGenre',
     'withUser',
-    {
-      method: [ 'withRecentComments', {
-        userId: user.id,
-        limit: 10,
-      } ],
+    { method: [ 'withRecentComments', { userId: user.id, limit: 10 } ],
     },
   ];
 
-  const options = {
+  const optionsPrioritized = {
     where: [
       {
         genreId: user.preferredGenreIds,
@@ -40,20 +38,44 @@ router.get('/', asyncMiddleware(async (request, response) => {
       [ 'createdAt', 'DESC' ],
     ],
     group: [ 'track.userId' ],
-    limit: 15,
+    limit: 6,
   };
 
-  const preferredTracks = await TrackModel.scope(scope).findAll(options);
+  const optionsNoComments = {
+    where: [
+      {
+        genreId: user.preferredGenreIds,
+        userId: { [Sequelize.Op.ne]: user.id },
+        totalComments: 0,
+        draft: false,
+      },
+      database.literal(`(track.id NOT IN (SELECT tp.trackId from trackPlays tp WHERE tp.userId = ${user.id}))`),
+    ],
+    order: [
+      database.literal('RAND()'),
+      [ 'createdAt', 'DESC' ],
+    ],
+    group: [ 'track.userId' ],
+    limit: 6,
+  };
 
-  if (preferredTracks.length) {
-    return response.success(preferredTracks);
+  const preferredTracksPrioritized = await TrackModel.scope(scope).findAll(optionsPrioritized);
+  const preferredTracksNoComments = await TrackModel.scope(scope).findAll(optionsNoComments);
+
+  if (preferredTracksPrioritized.length || preferredTracksNoComments.length) {
+    return response.success(shuffleArray([
+      ...preferredTracksPrioritized,
+      ...preferredTracksNoComments,
+    ]));
   }
 
-  delete options.where[0].genreId;
+  delete optionsPrioritized.where[0].genreId;
+  delete optionsNoComments.where[0].genreId;
 
-  const allTracks = await TrackModel.scope(scope).findAll(options);
-
-  response.success(allTracks);
+  response.success(shuffleArray([
+    ...(await TrackModel.scope(scope).findAll(optionsPrioritized)),
+    ...(await TrackModel.scope(scope).findAll(optionsNoComments)),
+  ]));
 }));
 
 /*
